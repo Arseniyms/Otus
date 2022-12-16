d93ca87bf2e99906ffcf37a3cd1e5be70e65e1b3
Arseniy <arsmatus@list.ru>
2022-12-16 14:23:34 +0300

    task 14

---
+++ Task 14. Animations/Task 14. Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task 14. Animations
+//
+//  Created by Arseniy Matus on 15.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
